0f1e7bbaacb545ba61db76de053740cbc244820d
mybna134 <mybna134@163.com>
2025-11-29 01:16:07 +0800

    build

---
+++ mkdeb.sh
@@ -0,0 +1,15 @@
+!#/bin/bash
+targetPath="LabelMaster/opt/labelmaster/labelmaster"
+binPath="bin/LabelMaster"
+if [ -f ${binPath} ]
+then
+	rm ${binPath}
+fi
+cmake --build build
+if [ -f ${targetPath} ]
+then 
+	rm ${targetPath}
+fi	
+mv ${binPath} ${targetPath}
+dpkg-deb --build LabelMaster labelmaster_1.1.0_amd64.deb
+mv labelmaster_1.1.0_amd64.deb ../dataset
